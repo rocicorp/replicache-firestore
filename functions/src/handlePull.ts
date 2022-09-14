c81d86d6520014b2027aa005586965a24b7c9787
Aaron Boodman <aaron@aaronboodman.com>
2022-09-13 23:09:38 -1000

    Update handlePull.ts

--- functions/src/handlePull.ts
+++ functions/src/handlePull.ts
@@ -29,16 +29,22 @@ export const pull = functions.https.onRequest(async (req, res) => {
       // Super mega important: all three values (lmid, cookie, patch) must be
       // read as part of the same snapshot.
 
+      // Read the LMID for the requesting client. This tells the client which
+      // of its pending mutations should be discarded.
       const lastMutationID = await getLastMutationID(db, tx, clientID);
       if (lastMutationID === undefined && reqLastMutationID > 0) {
         return { error: "Unknown client" };
       }
 
+      // Read the current version for the space requested. We set this as the
+      // Replicache "cookie". Next pull, Replicache will send this back. This
+      // allows us to return only changed keys.
       const cookie = await getVersion(db, tx, spaceID);
       if (cookie === undefined) {
         return { error: "Unknown space" };
       }
 
+      // Read all entries changed since the supplied cookie.
       const query = db
         .collection("todos")
         .where("spaceID", "==", spaceID)
@@ -62,6 +68,8 @@ export const pull = functions.https.onRequest(async (req, res) => {
 
   const patch = [];
   const { todos, cookie, lastMutationID } = txRes;
+  
+  // Build our patch
   for (const doc of todos.docs) {
     const entry = doc.data() as TodoEntry;
     const key = `todos/${entry.todo.id}`;
